3fe2ad06367ba892c165bb60cae800b3e1b0c97c
stellantilet <stellantilet@gmail.com>
2021-08-30 00:14:04 -0400

    correcting comment

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -27,7 +27,7 @@ import { Todo } from "../types";
 interface MyDOMInputEvent extends Event {
   target: EventTarget & {
     //for some reason value isn't a property on the vanilla EventTarget type given by typescript....
-    // so we are forcing it to be a value
+    // so we are forcing it to be a property
     value: number | string;
   };
 }
