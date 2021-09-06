bcb521d56f137d01054487cb255ce343a504583b
stellantilet <stellantilet@gmail.com>
2021-09-05 21:02:29 -0400

    reformatting test helpers

--- server/src/__tests__/utils/helpers.ts
+++ server/src/__tests__/utils/helpers.ts
@@ -1,12 +1,12 @@
-  
+
 import { ANSI_ESCAPES, EditTodoPayload } from "../../types";
 
 export function logJson(input: any): void {
-	return (() => console.log(`${ANSI_ESCAPES.yellow}`, `${JSON.stringify(input, null, 2)}`, `${ANSI_ESCAPES.reset}`))();
+  return (() => console.log(`${ANSI_ESCAPES.yellow}`, `${JSON.stringify(input, null, 2)}`, `${ANSI_ESCAPES.reset}`))();
 };
 
 export function createAddTodoMutation(creatorId: number): string {
-	return `
+  return `
 		mutation addTodo {
 			addTodo(options: {
 				text: "some text",
@@ -23,7 +23,7 @@ export function createAddTodoMutation(creatorId: number): string {
 }
 
 export function createGetUserTodosQuery(creatorId: number): string {
-	return `
+  return `
 		{
 			getUserTodos(creatorId: ${creatorId}){
 				id
@@ -37,7 +37,7 @@ export function createGetUserTodosQuery(creatorId: number): string {
 }
 
 export function createClearUserTodosMutation(creatorId: number): string {
-	return `
+  return `
 		mutation clearUserTodos {
 			clearUserTodos(creatorId: ${creatorId})
 		}
@@ -45,8 +45,8 @@ export function createClearUserTodosMutation(creatorId: number): string {
 }
 
 export function createEditTodoMutation(payload: EditTodoPayload): string {
-	const { text, id } = payload
-	return `
+  const { text, id } = payload
+  return `
 		mutation editTodoById{
 			editTodoById(options: {
 				text: "${text}",
